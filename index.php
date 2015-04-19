841cd1bbbb26ff6a63bd90854fa62ec4c8e18ba5
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2015-04-20 01:36:53 +0300

    added angular, init standard module and controller

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
-$url = $_GET['url'];
-echo file_get_contents($url);
+try {
+    $url = $_GET['url'];
+    echo @file_get_contents($url);
+} catch (Exception $e) {
+    echo $e->getMassage();
+}
--- js/replacement.js
+++ js/replacement.js
@@ -2,4 +2,36 @@
  * 1. Отправить head страницы в свой head, но предварительно запомнить, что-бы потом можно было удалить
  * 2. Переобразовать все относительные ссылки на абсолютные
  * 3. Изолировать свои классы и скрипты
+ * 4. css стили искомого сайта оборачивать в less и переобразовывать на лету
+ *
+ * разберать header и body нужно на сервере
+ *
  **/
+//
+var DomHelper = {
+    getHeader : function (html) {
+        var parser = new DOMParser(),
+            doc = parser.parseFromString(html, "text/xml");
+        console.log(doc);
+    }
+}
+
+var app = angular.module( "App", [] );
+app.controller(
+    "MyController",
+    function( $scope , $http, $sce) {
+
+        $scope.go = function () {
+            $http.get('/index.php?url='+$scope.url).
+                success(function(data, status, headers, config) {
+                    var html = $sce.trustAsHtml(data);
+                    var head = DomHelper.getHeader(data);
+                    // вставляем html
+                    //$scope.content = html;
+                }).
+                error(function(data, status, headers, config) {
+
+                });
+        }
+    }
+);
